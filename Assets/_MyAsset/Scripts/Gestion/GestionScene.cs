5cd3c7944cb7329ba311bcb1192ba83ec5355d78
Grondin Alexandre <117280087+Miro974@users.noreply.github.com>
2023-04-10 17:35:26 -0400

    Recherche font

---
+++ Assets/_MyAsset/Scripts/Gestion/GestionScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestionScene : MonoBehaviour
+{
+    public void ChargerSceneSuivante()
+    {
+        // Récupérer l'index de la scène en cours
+        int noScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(noScene + 1);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+
+    public void CahrgerSceneDepart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
